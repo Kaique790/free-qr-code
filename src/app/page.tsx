2e56ea48a8246aedd4697a500d23c0a429115bbf
Kaique790 <kiaqbs@gmail.com>
2025-11-10 21:00:06 -0300

    feat: enhance layout with responsive adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-import { ArrowRightIcon, ImageIcon } from "@phosphor-icons/react/ssr";
+import {
+  ArrowRightIcon,
+  CheckIcon,
+  ImageIcon,
+} from "@phosphor-icons/react/ssr";
 import heroImg from "@/assets/images/hero-mockup.png";
 
 import { Button } from "@/components/ui/buttons";
@@ -6,28 +10,34 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="mt-12 ml-auto grid max-w-[calc(100vw-(100vw-1140px)/2)] gap-8 md:grid-cols-2 md:grid-rows-[auto_1fr] md:items-start">
-      <div className="order-1 md:order-0 md:col-start-1 md:row-start-1">
-        <div className="space-y-3">
-          <h1 className="text-center text-[3.5rem] leading-none font-bold md:text-left">
+    <div className="mt-12 ml-auto grid max-w-[calc(100vw-(100vw-1200px)/2)] gap-8 min-[800px]:mt-17 min-[800px]:grid-cols-2 min-[800px]:grid-rows-[auto_1fr] min-[800px]:items-start">
+      <div className="px-8 min-[800px]:order-0 min-[800px]:col-start-1 min-[800px]:row-start-1">
+        <div className="flex items-center justify-center gap-2 text-sm opacity-35 min-[800px]:justify-start">
+          <span className="bg-primary rounded-full p-1">
+            <CheckIcon size={12} />
+          </span>
+          QRcode grátis
+        </div>
+        <div className="mt-4 space-y-3">
+          <h1 className="text-center text-[3.5rem] leading-none font-bold min-[800px]:text-left">
             Gere seu <span className="text-primary">QRcode</span> rapidamente.
           </h1>
-          <p className="text-center font-medium opacity-60 md:text-left">
+          <p className="text-center font-medium opacity-60 min-[800px]:text-left">
             Gere QRcode com seu logotipo. Um clique, 20 QRcodes!
           </p>
         </div>
 
         <Button
           variant="dark"
-          className="mx-auto mt-8 w-[330px] max-w-full rounded-full py-2 md:mx-0 md:mt-12"
+          className="mx-auto mt-8 w-[330px] max-w-full rounded-full py-2 min-[800px]:mx-0 min-[800px]:mt-12"
         >
           Gerar o meu <ArrowRightIcon />
         </Button>
       </div>
 
-      <div className="order-3 pl-8 min-[800px]:pl-0 md:order-0 md:col-start-2 md:row-span-2 md:row-start-1">
+      <div className="pl-8 min-[800px]:order-0 min-[800px]:col-start-2 min-[800px]:row-span-2 min-[800px]:row-start-1 min-[800px]:pl-0">
         <Image
-          className="mt-8 ml-auto w-[90%] max-w-full object-cover"
+          className="mt-8 ml-auto w-[90%] max-w-full object-cover min-[800px]:mt-0"
           src={heroImg}
           quality={100}
           width={1500}
@@ -36,10 +46,10 @@ export default function Home() {
         />
       </div>
 
-      <div className="order-2 px-8 md:order-0 md:col-start-1 md:row-start-2 md:mt-12 md:px-0">
-        <article className="border-dark mx-auto w-[380px] max-w-full overflow-hidden rounded-[2.5rem] border-2 md:mx-0 md:flex md:w-[520px]">
+      <div className="px-8 min-[800px]:order-0 min-[800px]:col-start-1 min-[800px]:row-start-2 min-[800px]:mt-12 min-[800px]:px-0 min-[800px]:pl-8">
+        <article className="border-dark mx-auto w-[380px] max-w-full overflow-hidden rounded-[2.5rem] border-2 min-[800px]:mx-0 min-[800px]:flex min-[800px]:w-[520px]">
           <div className="flex h-[244px] flex-col justify-between px-6 py-8">
-            <div className="text-center md:text-left">
+            <div className="text-center min-[800px]:text-left">
               <h3 className="text-2xl font-medium">QR + LOGO</h3>
               <p className="opacity-70">
                 você pode adicionar sua logo, só copiar e colar.
@@ -54,7 +64,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="bg-dark flex h-[244px] flex-col justify-between px-6 py-8 text-white md:min-w-[230px]">
+          <div className="bg-dark flex h-[244px] flex-col justify-between px-6 py-8 text-white min-[800px]:min-w-[230px]">
             <div className="text-center">
               <span className="text-5xl font-bold">100%</span>
               <p className="font-light">
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <div className="mx-auto flex w-full items-center gap-3 px-8 pt-20 md:gap-12">
+    <div className="mx-auto flex w-full max-w-[1220px] items-center gap-3 px-8 pt-20 md:gap-12">
       <div className="bg-dark mx-auto h-px w-full" />
       <p className="whitespace-nowrap">&copy; Developed by Kaique790</p>
       <div className="bg-dark mx-auto h-px w-full" />
