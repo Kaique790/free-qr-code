6eb5de67f451913ca0d3172141d93218e72840f7
Kaique790 <kiaqbs@gmail.com>
2025-11-17 17:31:27 -0300

    feat: add auth js config

---
+++ src/lib/auth.ts
@@ -0,0 +1,64 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./prisma";
+import bcrypt from "bcrypt";
+
+export const { auth, handlers, signIn } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+
+  session: {
+    strategy: "jwt",
+  },
+
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+
+    CredentialsProvider({
+      name: "credentials",
+
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+
+      async authorize(credentials) {
+        const cred = credentials as { email: string; password: string };
+
+        if (!cred.email || !cred.password) {
+          throw new Error("Preencha email e senha");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: cred.email },
+        });
+
+        if (!user || !user.password) {
+          throw new Error("User not found.");
+        }
+
+        const matchPassword = await bcrypt.compare(
+          String(cred.password),
+          user.password,
+        );
+
+        if (!matchPassword) {
+          throw new Error("Invalid credentials.");
+        }
+
+        return user;
+      },
+    }),
+  ],
+});
