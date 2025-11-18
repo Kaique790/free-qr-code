87e962a1216671911d030e2868a3d38da1baa765
Kaique790 <kiaqbs@gmail.com>
2025-11-18 20:25:02 -0300

    feat(auth): implement sign-in form with validation and error handling

--- src/app/auth/form/sign-in-form.tsx
+++ src/app/auth/form/sign-in-form.tsx
@@ -10,35 +10,90 @@ import {
 import Link from "next/link";
 import { useState } from "react";
 import { GoogleBtn } from "./google-btn";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+import { signIn } from "next-auth/react";
+
+const formSchema = z.object({
+  email: z.email("Digite um e-mail válido"),
+  password: z.string().min(8, "A senha contém no mínino 8 digitos."),
+});
+
+type SignInFormData = z.infer<typeof formSchema>;
 
 export function SignInForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInFormData>({
+    resolver: zodResolver(formSchema),
+  });
+
   const [showPassword, setShowPassword] = useState(false);
+
   function toggleShowPassword() {
     setShowPassword((state) => !state);
   }
 
+  async function onSubmit(data: SignInFormData) {
+    try {
+      signIn("credentials", {
+        email: data.email,
+        password: data.password,
+      });
+    } catch (err) {
+      console.error(err);
+      alert("Erro inesperado, tente novamente.");
+    }
+  }
+
   return (
-    <form className="space-y-6">
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
       <div className="flex flex-col gap-6">
         <div className="flex flex-col gap-2">
           <label htmlFor="email leading-none">Email</label>
-          <Input placeholder="ex: meuemail@gmail.com" id="email" type="email" />
-        </div>
-        <div className="relative overflow-hidden">
           <Input
-            placeholder="********"
-            id="password"
-            type={showPassword ? "text" : "password"}
-            className="w-full"
+            {...register("email")}
+            placeholder="ex: meuemail@gmail.com"
+            id="email"
+            type="email"
           />
-          <Button
-            onClick={toggleShowPassword}
-            variant={showPassword ? "outline" : "dark"}
-            type="button"
-            className="absolute top-0 right-0 flex h-full w-fit items-center gap-2 text-sm"
-          >
-            {showPassword ? <EyeIcon size={20} /> : <EyeClosedIcon size={20} />}
-          </Button>
+          {errors.email && (
+            <span className="text-sm text-rose-500">
+              {errors.email.message}
+            </span>
+          )}
+        </div>
+        <div className="flex flex-col gap-2 leading-none">
+          <label htmlFor="password">Senha</label>
+          <div className="relative overflow-hidden">
+            <Input
+              {...register("password")}
+              placeholder="********"
+              id="password"
+              type={showPassword ? "text" : "password"}
+              className="w-full"
+            />
+            <Button
+              onClick={toggleShowPassword}
+              variant={showPassword ? "outline" : "dark"}
+              type="button"
+              className="absolute top-0 right-0 flex h-full w-fit items-center gap-2 text-sm"
+            >
+              {showPassword ? (
+                <EyeIcon size={20} />
+              ) : (
+                <EyeClosedIcon size={20} />
+              )}
+            </Button>
+          </div>
+          {errors.password && (
+            <span className="text-sm text-rose-500">
+              {errors.password.message}
+            </span>
+          )}
         </div>
         <Button className="w-full" variant="dark">
           Entrar <ArrowRightIcon size={20} />
